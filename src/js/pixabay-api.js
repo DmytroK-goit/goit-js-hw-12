cf56b5562d89a39923009b0cef2ca3655c219193
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-07-17 17:00:31 +0300

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const loader = document.querySelector('.loader');
+const fetchPostsBtn = document.querySelector('.btn');
 
 export function showLoader() {
   loader.style.display = 'block';
@@ -9,10 +10,19 @@ export function hideLoader() {
   loader.style.display = 'none';
 }
 
-export function performSearch(searchQuery) {
-  const API_KEY = '44758497-ea11318ae0823ef09cb8fbdb5';
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
-    searchQuery
-  )}&image_type=photo`;
-  return fetch(URL);
+let page = 1;
+const perPage = 20;
+const API_KEY = '44758497-ea11318ae0823ef09cb8fbdb5';
+
+export async function performSearch(searchQuery) {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: searchQuery,
+    image_type: 'photo',
+    per_page: perPage,
+    page: page,
+  });
+
+  const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
+  return response.data.hits;
 }
--- src/main.js
+++ src/main.js
@@ -6,57 +6,72 @@ import { performSearch, showLoader, hideLoader } from './js/pixabay-api.js';
 import { renderImages } from './js/render-functions';
 
 const searchForm = document.querySelector('#search-form');
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('submit', async event => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const searchQuery = formData.get('query');
 
   if (searchQuery && searchQuery.trim()) {
-    showLoader();
-    performSearch(searchQuery.trim())
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Не вдалося виконати запит.');
-        }
-        return response.json();
-      })
-      .then(data => {
-        hideLoader();
-        if (data.totalHits > 0) {
-          iziToast.show({
-            position: 'topRight',
-            backgroundColor: 'green',
-            message: `Found ${data.totalHits} results.`,
-          });
-          return data.hits;
-        } else {
-          iziToast.show({
-            position: 'topRight',
-            backgroundColor: 'red',
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-          });
-          return [];
-        }
-      })
-      .then(results => {
-        console.log(results);
-        renderImages(results);
-      })
-      .catch(error => {
-        hideLoader(),
-          iziToast.show({
-            position: 'topRight',
-            backgroundColor: 'red',
-            message: 'Error during the request. Please try again later.',
-          });
-        throw error;
-      });
-  } else {
-    iziToast.show({
-      position: 'center',
-      backgroundColor: 'orange',
-      message: 'Будь ласка, введіть пошуковий запит.',
-    });
+    try {
+      showLoader();
+      const posts = await performSearch(searchQuery.trim());
+      renderImages(posts);
+      //   page += 1;
+      //   if (page > 1) {
+      //     fetchPostsBtn.textContent = 'Fetch more posts';
+      //   }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      hideLoader();
+    }
   }
 });
+//     showLoader();
+//     performSearch(searchQuery.trim())
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Не вдалося виконати запит.');
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         hideLoader();
+//         if (data.totalHits > 0) {
+//           iziToast.show({
+//             position: 'topRight',
+//             backgroundColor: 'green',
+//             message: `Found ${data.totalHits} results.`,
+//           });
+//           return data.hits;
+//         } else {
+//           iziToast.show({
+//             position: 'topRight',
+//             backgroundColor: 'red',
+//             message:
+//               'Sorry, there are no images matching your search query. Please try again!',
+//           });
+//           return [];
+//         }
+//       })
+//       .then(results => {
+//         console.log(results);
+//         renderImages(results);
+//       })
+//       .catch(error => {
+//         hideLoader(),
+//           iziToast.show({
+//             position: 'topRight',
+//             backgroundColor: 'red',
+//             message: 'Error during the request. Please try again later.',
+//           });
+//         throw error;
+//       });
+//   } else {
+//     iziToast.show({
+//       position: 'center',
+//       backgroundColor: 'orange',
+//       message: 'Будь ласка, введіть пошуковий запит.',
+//     });
+//   }
+// });
